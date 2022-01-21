16d2926be866d3fa4ab335c850aa64d13b5bfa80
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-01-20 21:09:37 -0500

    api routes for candidates and parties

--- server.js
+++ server.js
@@ -105,6 +105,33 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
+app.put('/api/candidate/:id', (req, res) => {
+  const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+  const errors = inputCheck(req.body, 'party_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  const params = [req.body.party_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
 app.get('/api/parties', (req, res) => {
   const sql = `SELECT * FROM parties`;
   db.query(sql, (err, rows) => {
